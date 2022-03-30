a8b0855b59d8bca8f14d2772048d9c17a8559f4b
aleexalvz <alex.alves_br@hotmail.com>
2022-03-30 09:11:54 -0300

    primeira versão da ui pokedex

---
+++ app/src/main/java/com/aleexalvz/pokedex/ui/PokedexActivity.kt
@@ -0,0 +1,14 @@
+package com.aleexalvz.pokedex.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.aleexalvz.pokedex.R
+
+class PokedexActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokedex)
+    }
+
+}
