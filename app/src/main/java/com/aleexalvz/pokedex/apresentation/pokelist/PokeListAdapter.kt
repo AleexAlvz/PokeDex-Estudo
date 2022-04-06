8e52f232e9af19e2f2df0418a37de33e21d5cb2a
aleexalvz <alex.alves_br@hotmail.com>
2022-04-05 22:56:01 -0300

    adiciona dependencia Glide e insere as imagens de pokemons na tela

--- app/src/main/java/com/aleexalvz/pokedex/apresentation/pokelist/PokeListAdapter.kt
+++ app/src/main/java/com/aleexalvz/pokedex/apresentation/pokelist/PokeListAdapter.kt
@@ -4,16 +4,18 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.aleexalvz.pokedex.R
 import com.aleexalvz.pokedex.data.model.Pokemon
+import com.bumptech.glide.Glide
 
 class PokeListAdapter(
-    val context: Context
+    private val context: Context
 ) : RecyclerView.Adapter<PokeListAdapter.PokeListViewHolder>() {
 
-    val pokeList = mutableListOf<Pokemon>()
+    private val pokeList = mutableListOf<Pokemon>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokeListViewHolder {
         val layout = LayoutInflater.from(context).inflate(R.layout.item_pokemon, parent, false)
@@ -23,9 +25,16 @@ class PokeListAdapter(
     override fun getItemCount(): Int = pokeList.size
 
     override fun onBindViewHolder(holder: PokeListViewHolder, position: Int) {
-        holder.name.text = pokeList[position].name
-        val pokemonNumber = "#${pokeList[position].index}"
+        val pokemon = pokeList[position]
+
+        holder.name.text = pokemon.name
+
+        val pokemonNumber = "#${pokemon.index}"
         holder.number.text = pokemonNumber
+
+        if (pokemon.spriteURL.isNotBlank()){
+            Glide.with(context).load(pokemon.spriteURL).into(holder.image)
+        }
     }
 
     fun addPokemon(pokemon: Pokemon) {
@@ -33,7 +42,8 @@ class PokeListAdapter(
     }
 
     class PokeListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val number = itemView.findViewById<TextView>(R.id.item_pokemon_number)
-        val name = itemView.findViewById<TextView>(R.id.item_pokemon_name)
+        val number: TextView = itemView.findViewById(R.id.item_pokemon_number)
+        val name: TextView = itemView.findViewById(R.id.item_pokemon_name)
+        val image: ImageView = itemView.findViewById(R.id.item_pokemon_image)
     }
 }
